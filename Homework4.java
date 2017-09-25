6157b5d9d18dcdc293b64798f0ebbc6958718ebf
aleksz <ivan12228@gmail.com>
2017-09-25 23:48:29 +0300

    Исправил ошибки

--- Homework4.java
+++ Homework4.java
@@ -1,23 +1,32 @@
 import java.util.Scanner;
 
+import static java.lang.System.*;
+
 public class Homework4 {
-
-/* 1
     public static void main(String[] args) {
-        int x = 10;
-        int count=x;
+
+        drawRectangle();
+        drawRectangleOneSize(3, '+');
+        function();
+        getMax(15, 8);
+        recursion(10);
+        System.out.println(recursion(10));
+        drawRectRecursion(5, 4);
+    }
+
+    public static void function() {
+        int x = 3;
+        int count = x;
         {
-        for (int i=1;i <= x;i++){
-        System.out.println(i + "\n");
+            for (int i = 1; i <= x; i++) {
+                System.out.println(i + "\n");
+            }
         }
-        }
-        }
-*/
+    }
 
-/* 2
-    public static void main(String[] args) {
+    public static void drawRectangle() {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Высота прямоугольника::");
+        System.out.println("Высота прямоугольника:");
         int a = sc.nextInt();
         System.out.println("Ширина прямоугольника:");
         int b = sc.nextInt();
@@ -36,77 +45,68 @@ public class Homework4 {
         }
     }
 
-/* 3
-    static String drawRectangle(int width, char c){
+
+    static void drawRectangleOneSize(int width, char c) {
         String result = "";
         for (int i = 0; i < width; i++) {
             String line = "";
             for (int j = 0; j < width; j++)
                 line += c;
-        }
-        result += line + "\n";
-    }
-        return result;
-}
-
-    public static void main(String[] args) {
-        System.out.println(drawRectangle(10, '+'));
-    }
-
-*/
-
-
-/* 4
-         public static void main(String[] args) {
-        getMax(15, 8);
-        }
-        static int getMax(int a, int b ){
-        if(a>b) {
-        System.out.println(a + " Больше");
-        return a;
-        }
-        if (a<b) {
-        System.out.println(b + " Больше");
-        return b;
-        }
-        if(a==b)
-        return a;
-        return a;
-        }
-*/
-
-
-/* 5
-        public static String recursion(int n) {
-        if (n == 1) {
-        return "1";
-        }
-        return recursion(n - 1) + "" + n;
-        }
-        public static void main(String[] args){
-        System.out.println(recursion(20));
-        }
-*/
-
-/* 6
-   static String drawRectangle(int width, int height, char c) {
-        String result = "";
-        for (int i = 0; i < height; i++) {
-            String line = "";
-            for (int j = 0; j < width; j++) {
-                line += c;
-            }
             result += line + "\n";
         }
-        return result;}
-
-   public static void main(String[] args) {
-        System.out.println(drawRectangle(3, 5, '+'));
+        System.out.print(result);
     }
 
-*/
 
+    public static void Getmax(String[] args) {
+        getMax(15, 8);
     }
+
+    static int getMax(int a, int b) {
+        if (a > b) {
+            System.out.println(a + " Больше");
+            return a;
+        }
+        if (a < b) {
+            System.out.println(b + " Больше");
+            return b;
+        }
+        if (a == b)
+            return a;
+        return a;
+    }
+
+    public static String recursion(int n) {
+        if (n == 1) {
+            return "1";
+        }
+        return recursion(n - 1) + "" + n;
+    }
+    public static void drawRectRecursion(int w, int h){
+        if (h == 0) return;
+        drawRow(w);
+        drawRectRecursion(w, h - 1);
+    }
+
+    public static void drawRow(int w){
+        if(w == 0) {
+            System.out.print("\n");
+            return;
+        }
+        System.out.print("+ ");
+        drawRow(w - 1);
+    }
+    }
+
+
+
+
+
+
+
+
+
+
 
 
 
